47fb9b19022ffa8c3338998b3f8ca611910bde2b
Joao Fernando <joaofbrigido@hotmail.com>
2021-01-31 20:31:19 -0300

    Animação ao Scroll adicionada

--- src/script.js
+++ src/script.js
@@ -17,7 +17,44 @@ function scrollSuave() {
 }
 scrollSuave();
 
-// function animaAoScroll() {
-//   const sections = document.querySelectorAll('[data-anima]');
-// }
-// animaAoScroll();
+function animaAoScroll() {
+  const debounce = function (func, wait, immediate) {
+    let timeout;
+    return function (...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  const sections = document.querySelectorAll('[data-anima]');
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.8;
+    const animaClass = 'anima';
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add(animaClass);
+        else if (section.classList.contains(animaClass))
+          section.classList.remove(animaClass);
+      });
+    }
+    animaScroll();
+
+    window.addEventListener(
+      'scroll',
+      debounce(() => {
+        animaScroll();
+      }, 50),
+    );
+  }
+}
+animaAoScroll();
